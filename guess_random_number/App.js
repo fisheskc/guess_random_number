b183077290c9cd396c6f2b29ee3148a1b65746ca
Karen Collins <fisheskc@users.noreply.github.com>
2020-01-12 19:55:00 +0000

    Add files via upload

---
+++ guess_random_number/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Game from './Game';
+import "./game.css";
+
+const App = () => {
+    return (
+        <>
+            <div>
+                <Game />
+                <h4>random number is:</h4>
+            </div>
+        </>
+    )
+}
+
+export default App;
